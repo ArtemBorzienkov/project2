64809dc92b36157978fc6c1199ed4f51b53ee008
Artem <artemborzienkov@gmail.com>
2018-05-11 10:09:19 +0200

    add ex2.html

---
+++ ex2js.js
@@ -0,0 +1,11 @@
+const onAddUserButtonClick = (event) => {
+    var userName=prompt("User Name",null);
+    var password=prompt("password",null);
+    var firstName=prompt("First Name",null);
+    var lastName=prompt("Last Name",null);
+    var birth=prompt("Date of birth",null);
+    var groups=prompt("List of User Groups to which the User belongs",null);
+}
+
+var list1=[onAddUserButtonClick.userName, onAddUserButtonClick.password, onAddUserButtonClick.firstName,
+   onAddUserButtonClick.lastName, onAddUserButtonClick.birth, onAddUserButtonClick.groups];
